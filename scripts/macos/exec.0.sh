de73619d064a33c58c5cb70a1ed5b5266b51d787
Oleksandr B <balyszyn@gmail.com>
2023-07-04 13:23:11 +0300

    Update exec.0.sh

--- scripts/macos/exec.0.sh
+++ scripts/macos/exec.0.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
