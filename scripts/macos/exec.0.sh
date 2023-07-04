2f23fc616844072979a1b5ba502abe5ebc3e6c2d
Oleksandr B <balyszyn@gmail.com>
2023-07-04 13:04:43 +0300

    Update exec.0.sh

--- scripts/macos/exec.0.sh
+++ scripts/macos/exec.0.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-brew install install fzf wget
-brew install zlib
-brew install cmake make gcc meson
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
+
+brew install install fzf wget
 brew install zlib
 brew install cmake make gcc meson
 
