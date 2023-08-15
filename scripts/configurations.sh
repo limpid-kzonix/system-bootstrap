c6ad504c9daab14e9009b5cf0281ac90e2d26c89
limpid-kzonix <balyszyn@gmail.com>
2023-08-15 18:57:55 +0300

    drop conda releated stuff

--- scripts/configurations.sh
+++ scripts/configurations.sh
@@ -4,10 +4,9 @@ git config --global user.email  "balyszyn@gmail.com"
 git config --global user.name   "limpid-kzonix"
 git config --global pull.rebase true
 echo "Disable conda PS"
-conda config --set changeps1 False
 echo "Alacritty - default"
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator alacritty 50
-sudo update-alternatives --config x-terminal-emulator
+# sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator alacritty 50
+# sudo update-alternatives --config x-terminal-emulator
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
