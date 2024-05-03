d80a9dfda4dae9ee1074eb059cfccb5a4fb6ef95
limpid-kzonix <balyszyn@gmail.com>
2024-05-03 23:57:50 +0300

    update fedora installation order

--- scripts/fedora/exec.1.sh
+++ scripts/fedora/exec.1.sh
@@ -6,14 +6,8 @@ sudo dnf -y install vlc
 sudo dnf -y install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
 sudo dnf -y install ffmpeg-free
 
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing
-sudo dnf groupupdate multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-
-# hardware codecs for AMD
-sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
-sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
-
-sudo dnf install libva libva-utils gstreamer1-vaapi ffmpeg mesa-dri-drivers mpv
+sudo dnf install libva-utils
+sudo dnf install libva libva-utils gstreamer1-vaapi mesa-dri-drivers mpv
 sudo dnf install radeontop
 sudo dnf install libva-vdpau-driver libvdpau-va-gl libva-utils
 
@@ -24,7 +18,7 @@ sudo dnf -y install gnome-tweaks gnome-extensions-app
 #others
 sudo dnf install -y llvm jq meson g++ gcc clang make cmake
 sudo dnf install -y meson 
-sudo dnf install -y flex bison gperf cmake ninja-build ccache libffi-dev libssl-dev dfu-util
+sudo dnf install -y flex bison gperf cmake ninja-build ccache libffi-devel openssl-devel dfu-util
 sudo dnf install -y build-essential libreadline-dev unzip
 sudo dnf install -y stacer
 sudo dnf install -y xsensors
@@ -45,4 +39,25 @@ sudo dnf install -y v4l2loopback
 sudo dnf install -y obs-studio
 sudo dnf install -y obs-studio-libs
 
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing
+sudo dnf groupupdate multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 
+# hardware codecs for AMD
+sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+
+# Hardware codecs with AMD (mesa)
+
+# Using the rpmfusion-free section This is needed since Fedora 37 and later... and mainly concern AMD hardware since NVIDIA hardware with nouveau doesn't work well
+
+sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+
+# If using i686 compat libraries (for steam or alikes):
+
+sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
+sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
+
+sudo dnf install rpmfusion-nonfree-release-tainted
+sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware"
