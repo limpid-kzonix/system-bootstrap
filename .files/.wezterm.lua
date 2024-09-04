b3f6e4a9af7aa4b3a3f6ac9091c6fc5017e8e106
limpid-kzonix <balyszyn@gmail.com>
2024-09-04 20:38:56 +0300

    update wezterm config

--- .files/.wezterm.lua
+++ .files/.wezterm.lua
@@ -1,6 +1,9 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
+local act = wezterm.action
+
+
 -- This table will hold the configuration.
 local config = {}
 
@@ -10,7 +13,73 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+config.window_decorations = "NONE"
+config.integrated_title_button_style = "Gnome"
+config.integrated_title_buttons = { 'Hide', 'Maximize', 'Close' }
 -- This is where you actually apply your config choices
+
+config.color_scheme = 'Dark Ocean (terminal.sexy)'
+
+config.window_frame = {
+  inactive_titlebar_bg = '#353535',
+  active_titlebar_bg = '#2b2042',
+  inactive_titlebar_fg = '#cccccc',
+  active_titlebar_fg = '#ffffff',
+  inactive_titlebar_border_bottom = '#2b2042',
+  active_titlebar_border_bottom = '#2b2042',
+  button_fg = '#cccccc',
+  button_bg = '#2b2042',
+  button_hover_fg = '#ffffff',
+  button_hover_bg = '#3b3052',
+  font = require('wezterm').font 'Roboto',
+  font_size = 12,
+}
+
+config.window_frame = {
+  border_left_width = '0.10cell',
+  border_right_width = '0.10cell',
+  border_bottom_height = '0.05cell',
+  border_top_height = '0.05cell',
+  border_left_color = 'red',
+  border_right_color = 'red',
+  border_bottom_color = 'red',
+  border_top_color = 'red',
+}
+
+config.colors = {
+  tab_bar = {
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#575757',
+  },
+}
+
+config.window_background_opacity = 0.7
+config.text_background_opacity = 1
+
+config.mouse_wheel_scrolls_tabs = true
+config.mouse_bindings = {
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'CTRL',
+    action = act.IncreaseFontSize,
+  },
+
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'CTRL',
+    action = act.DecreaseFontSize,
+  },
+
+  {
+    event = { Down = { streak = 1, button = "Middle" } },
+    mods = 'CTRL',
+    action = act.ResetFontSize,
+  },
+}
+
+config.keys = {
+  { key = '0', mods = 'CTRL', action = wezterm.action.ResetFontSize },
+}
 
 -- For example, changing the color scheme:
 config.font = wezterm.font 'FiraCode Nerd Font'
@@ -25,8 +94,7 @@ config.set_environment_variables = {
 -- Spawn a fish shell in login mode
 -- config.default_prog = { 'zellij' }
 
-
-
+config.use_fancy_tab_bar = true
 
 -- and finally, return the configuration to wezterm
 return config
