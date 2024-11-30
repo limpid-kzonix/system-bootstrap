1675de1d81fb26f6fe3c1b72f308401110e0d8f9
limpid-kzonix <balyszyn@gmail.com>
2024-12-01 00:08:09 +0200

    chore(automation): Update installation scripts

--- scripts/cli-tools.sh
+++ scripts/cli-tools.sh
@@ -4,7 +4,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 curl -s "https://get.sdkman.io" | bash 
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+curl -sSL https://install.python-poetry.org | python3 -
 
 curl -sS https://starship.rs/install.sh | sh
 
