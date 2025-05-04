7af8c97b66d47077e9de70ee9b741b2a30cdcfbb
limpid-kzonix <balyszyn@gmail.com>
2025-05-04 22:25:09 +0300

    Updates flatpak installation commands
    Removes "--user" flag from flatpak installation commands

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -1,38 +1,39 @@
 #!/usr/bin/env zsh
 
-flatpak install flathub org.telegram.desktop -y --user
-flatpak install flathub com.discordapp.Discord -y --user
-flatpak install flathub com.google.Chrome -y --user
-flatpak install flathub com.spotify.Client -y --user
-flatpak install flathub com.brave.Browser -y --user
-flatpak install flathub org.inkscape.Inkscape -y --user
-flatpak install flathub org.audacityteam.Audacity -y --user
-flatpak install flathub rest.insomnia.Insomnia -y --user
-flatpak install flathub com.mattjakeman.ExtensionManager -y --user
+flatpak install flathub org.telegram.desktop -y 
+flatpak install flathub com.discordapp.Discord -y 
+flatpak install flathub com.google.Chrome -y 
+flatpak install flathub com.spotify.Client -y 
+flatpak install flathub com.brave.Browser -y 
+flatpak install flathub org.inkscape.Inkscape -y 
+flatpak install flathub org.audacityteam.Audacity -y 
+flatpak install flathub rest.insomnia.Insomnia -y 
+flatpak install flathub com.mattjakeman.ExtensionManager -y 
 
-flatpak install flathub com.github.tchx84.Flatseal -y --user
-flatpak install flathub io.github.giantpinkrobots.flatsweep -y --user
-flatpak install flathub io.github.flattool.Warehouse -y --user
+flatpak install flathub com.github.tchx84.Flatseal -y 
+flatpak install flathub io.github.giantpinkrobots.flatsweep -y 
+flatpak install flathub io.github.flattool.Warehouse -y 
 
-flatpak install flathub org.kde.kdenlive -y --user
-flatpak install flathub org.kde.krita -y --user
-flatpak install flathub io.missioncenter.MissionCenter -y --user
-flatpak install flathub org.wezfurlong.wezterm -y --user
-flatpak install flathub net.nokyan.Resources -y --user
-flatpak install flathub dev.zed.Zed -y --user
+flatpak install flathub org.kde.kdenlive -y 
+flatpak install flathub org.kde.krita -y 
+flatpak install flathub io.missioncenter.MissionCenter -y 
+flatpak install flathub org.wezfurlong.wezterm -y 
+flatpak install flathub net.nokyan.Resources -y 
+flatpak install flathub dev.zed.Zed -y 
 
-flatpak install flathub io.gitlab.liferooter.TextPieces -y --user
-flatpak install flathub app.devsuite.Ptyxis -y --user
-flatpak install flathub org.gnome.dspy -y --user
-flatpak install flathub page.tesk.Refine -y --user
+flatpak install flathub io.gitlab.liferooter.TextPieces -y 
+flatpak install flathub app.devsuite.Ptyxis -y 
+flatpak install flathub org.gnome.dspy -y 
+flatpak install flathub page.tesk.Refine -y 
 
-flatpak install flathub org.kde.audiotube -y --user
+flatpak install flathub org.kde.audiotube -y 
 
-flatpak install flathub org.gnome.Polari -y --user
+flatpak install flathub org.gnome.Polari -y 
 
+flatpak install flathub net.nokyan.Resources -y 
+flatpak install flathub io.github.mrvladus.List -y 
+flatpak install flathub com.belmoussaoui.Authenticator -y 
+flatpak install flathub org.gnome.gitlab.somas.Apostrophe -y 
+flatpak install flathub com.vscodium.codium -y 
+flatpak install flathub org.zulip.Zulip -y 
 
-flatpak install flathub net.nokyan.Resources -y --user
-flatpak install flathub io.github.mrvladus.List -y --user
-flatpak install flathub com.belmoussaoui.Authenticator -y --user
-flatpak install flathub org.gnome.gitlab.somas.Apostrophe -y --user
-flatpak install flathub com.vscodium.codium -y --user
