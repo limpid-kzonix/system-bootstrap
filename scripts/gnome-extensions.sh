72f493f2f27e4dbfe7d8c108430aafc9c2518d76
limpid-kzonix <balyszyn@gmail.com>
2025-05-05 00:15:53 +0300

    chore(update): additional gnome-extensions

--- scripts/gnome-extensions.sh
+++ scripts/gnome-extensions.sh
@@ -13,4 +13,6 @@ gext install user-theme@gnome-shell-extensions.gcampax.github.com
 gext install battery-indicator-icon@Deminder
 gext install just-perfection-desktop@just-perfection
 gext install notification-icons@muhammad_ans.github
-
+gext install tophat@fflewddur.github.io
+gext install space-bar@luchrioh
+gext install AlphabeticalAppGrid@stuarthayhurst
