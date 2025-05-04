9acbc1f6df16ca9810efc8a91963402b0b7ccd0f
limpid-kzonix <balyszyn@gmail.com>
2025-05-04 23:50:16 +0300

    chore(update): fix of dummy mistakes

--- scripts/gnome-extensions.sh
+++ scripts/gnome-extensions.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env zsh
 
+if command -v gext >/dev/null 2>&1; then 
+  echo "Installing gnome extensions"
+else 
+  pipx install gext
+fi 
+
 gext install dash-to-dock@micxgx.gmail.com
-gext status-icons@gnome-shell-extensions.gcampax.github.com
-gext tilingshell@ferrarodomenico.com
-gext user-theme@gnome-shell-extensions.gcampax.github.com
+gext install status-icons@gnome-shell-extensions.gcampax.github.com
+gext install tilingshell@ferrarodomenico.com
+gext install user-theme@gnome-shell-extensions.gcampax.github.com
+gext install battery-indicator-icon@Deminder
+gext install just-perfection-desktop@just-perfection
+gext install notification-icons@muhammad_ans.github
 
