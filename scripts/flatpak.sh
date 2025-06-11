3e668372f2ed793721b1271f2f8c3581bb8415c3
limpid-kzonix <balyszyn@gmail.com>
2025-06-11 22:45:34 +0300

    chore(improvemets): rearrange the flatpak packages

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -1,48 +1,59 @@
 #!/usr/bin/env zsh
 
+# Internet Browsers
+flatpak install flathub io.gitlab.librewolf-community -y
+flatpak install flathub com.google.Chrome -y 
+flatpak install flathub com.brave.Browser -y 
+flatpak install flathub dev.zed.Zed -y 
+
+# Communication
 flatpak install flathub org.telegram.desktop -y 
 flatpak install flathub com.discordapp.Discord -y 
-flatpak install flathub com.google.Chrome -y 
-flatpak install flathub com.spotify.Client -y 
-flatpak install flathub com.brave.Browser -y 
-flatpak install flathub org.inkscape.Inkscape -y 
-flatpak install flathub org.audacityteam.Audacity -y 
-flatpak install flathub rest.insomnia.Insomnia -y 
-flatpak install flathub com.mattjakeman.ExtensionManager -y 
+flatpak install flathub org.zulip.Zulip -y
 
+# Media
+flatpak install flathub com.spotify.Client -y 
+flatpak install flathub org.audacityteam.Audacity -y 
+flatpak install flathub org.kde.audiotube -y 
+
+# Flatpak utils
+flatpak install flathub com.mattjakeman.ExtensionManager -y 
 flatpak install flathub com.github.tchx84.Flatseal -y 
 flatpak install flathub io.github.giantpinkrobots.flatsweep -y 
 flatpak install flathub io.github.flattool.Warehouse -y 
-
-flatpak install flathub org.kde.kdenlive -y 
-flatpak install flathub org.kde.krita -y 
-flatpak install flathub io.missioncenter.MissionCenter -y 
-flatpak install flathub org.wezfurlong.wezterm -y 
 flatpak install flathub net.nokyan.Resources -y 
-flatpak install flathub dev.zed.Zed -y 
+
+# Tools for creators
+flatpak install flathub org.kde.kdenlive -y 
+flatpak install flathub org.inkscape.Inkscape -y 
+flatpak install flathub org.kde.krita -y 
+flatpak install flathub io.github.nokse22.Exhibit -y
+flatpak install flathub org.blender.Blender -y 
+flatpak install flathub org.freecad.FreeCAD -y
+
 
 flatpak install flathub io.gitlab.liferooter.TextPieces -y 
 flatpak install flathub app.devsuite.Ptyxis -y 
+flatpak install flathub org.wezfurlong.wezterm -y 
 flatpak install flathub org.gnome.dspy -y 
+
+# DevTools
+flatpak install flathub rest.insomnia.Insomnia -y 
+flatpak install flathub com.vscodium.codium -y 
+
+# System Utils
 flatpak install flathub page.tesk.Refine -y 
-
-flatpak install flathub org.kde.audiotube -y 
-
-flatpak install flathub org.gnome.Polari -y 
-
-flatpak install flathub net.nokyan.Resources -y 
+flatpak install flathub io.missioncenter.MissionCenter -y 
+flatpak install flathub be.alexandervanhee.gradia -y
 flatpak install flathub io.github.mrvladus.List -y 
 flatpak install flathub com.belmoussaoui.Authenticator -y 
 flatpak install flathub org.gnome.gitlab.somas.Apostrophe -y 
-flatpak install flathub com.vscodium.codium -y 
+flatpak install flathub org.gnome.Polari -y 
+flatpak install flathub net.nokyan.Resources -y 
 
-flatpak install flathub org.zulip.Zulip -y
-flatpak install flathub io.github.nokse22.Exhibit -y
-
-flatpak install flathub org.zulip.Zulip -y 
+# Others
 flatpak install flathub org.gnome.gitlab.ilhooq.Bookup -y
 flatpak install flathub org.nickvision.money -y
 flatpak install flathub io.gitlab.news_flash.NewsFlash -y
 
-flatpak install flathub be.alexandervanhee.gradia -y
 
