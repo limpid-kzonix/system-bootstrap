f231c750522351adc02a516c12cb500a56e687ee
limpid-kzonix <balyszyn@gmail.com>
2025-07-21 00:54:13 +0300

    chore(assets): update the background images for Gnome

--- scripts/fedora/dotfiles.sh
+++ scripts/fedora/dotfiles.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env zsh
 
+rm -rf ~/.local/share/gnome-background-properties
+mkdir -p ~/.local/share/gnome-background-properties
+ln -s $(pwd)/../../.files/gnome-background-properties.xml  ~/.local/share/gnome-background-properties/adwaita.xml.in
+
 rm -rf ~/system-update.sh
 ln -s $(pwd)/../../.files/fedora-system-update.sh ~/system-update.sh
