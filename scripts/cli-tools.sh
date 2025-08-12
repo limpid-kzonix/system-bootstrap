7b2ee7e3eb5f89ac0a88a3e9014c4cb3ec8c3529
limpid-kzonix <balyszyn@gmail.com>
2025-08-12 22:01:48 +0300

    chore(cli-tools): add astral.uv

--- scripts/cli-tools.sh
+++ scripts/cli-tools.sh
@@ -20,3 +20,6 @@ curl -sfS https://dotenvx.sh\?directory\=/home/worxbend/.local/bin | sh
 
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh
+rm Miniforge3-$(uname)-$(uname -m).sh
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
